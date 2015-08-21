3119a8e029ec238863fcc42365c110ac2f994e05
Benjamin <zwergenkoenig_ks@web.de>
2015-08-21 22:36:59 +0200

    -Model Struktur optimiert

--- script/steckdose.js
+++ script/steckdose.js
@@ -1,24 +1,27 @@
 /// <reference path="../typings/knockout/knockout.d.ts"/>
-function Steckdose(code, id, name) {
+function Steckdose(frequency, id, name) {
     var self = this;
-    self.code = code;
+
     self.id = id;
-	self.name=name;
-	self.codeOn=code+" "+id+" 1";
-	self.codeOff=code+" "+id+" 0";
-}			
+    self.frequency = frequency;
+    self.name = name;
+
+    self.code = frequency + " " + self.id;
+    self.codeOn = self.code + " 1";
+    self.codeOff = self.code + " 0";
+}
 
 function SteckdosenModel() {
-	var self = this;
-	
-	self.steckdosen = ko.observableArray([
-        new Steckdose('01100', '1', 'TV Wohnzimmer'),
-        new Steckdose('01100', '2', 'XBOX Multimedia'),
-        new Steckdose('01100', '3', 'TV Schlafzimmer'),
-        
-		new Steckdose('01011', '1', 'Daniel A' ),
-        new Steckdose('01011', '2', 'Daniel B' ),
-        new Steckdose('01011', '3', 'Daniel C' )
+    var self = this;
+
+    self.steckdosen = ko.observableArray([
+        new Steckdose("01100", "1", "TV Wohnzimmer"),
+        new Steckdose("01100", "2", "XBOX Multimedia"),
+        new Steckdose("01100", "3", "TV Schlafzimmer"),
+
+        new Steckdose("01011", "1", "Daniel A"),
+        new Steckdose("01011", "2", "Daniel B"),
+        new Steckdose("01011", "3", "Daniel C")
     ]);
 }
 
