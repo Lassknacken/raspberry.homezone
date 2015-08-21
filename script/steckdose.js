8b0950afa0af09f107fae55796f19f73dc3c1b2f
Benjamin <zwergenkoenig_ks@web.de>
2015-08-21 23:18:57 +0200

    -klassen eingebunden

--- script/steckdose.js
+++ script/steckdose.js
@@ -1,4 +1,3 @@
-/// <reference path="../typings/knockout/knockout.d.ts"/>
 function Steckdose(frequency, id, name) {
     var self = this;
 
@@ -10,19 +9,3 @@ function Steckdose(frequency, id, name) {
     self.codeOn = self.code + " 1";
     self.codeOff = self.code + " 0";
 }
-
-function SteckdosenModel() {
-    var self = this;
-
-    self.steckdosen = ko.observableArray([
-        new Steckdose("01100", "1", "TV Wohnzimmer"),
-        new Steckdose("01100", "2", "XBOX Multimedia"),
-        new Steckdose("01100", "3", "TV Schlafzimmer"),
-
-        new Steckdose("01011", "1", "Daniel A"),
-        new Steckdose("01011", "2", "Daniel B"),
-        new Steckdose("01011", "3", "Daniel C")
-    ]);
-}
-
-ko.applyBindings(new SteckdosenModel());
